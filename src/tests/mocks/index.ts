80faa02b7337a6d6830b22355a331bcfde3f1cd4
Stefan Stoilov <stefanstoilov11@icloud.com>
2025-01-10 12:46:12 +0200

    Resolve vercel build error.

--- src/tests/mocks/index.ts
+++
@@ -1,8 +0,0 @@
-import { env } from "@/env";
-
-export async function enableMocking() {
-  if (env.NEXT_PUBLIC_ENABLE_API_MOCKING) {
-    const { worker } = await import("./browser");
-    return worker.start();
-  }
-}
