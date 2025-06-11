fd9cc8d732d8d5084ada85c30696a86670382f12
Stefan Stoilov <stefanstoilov11@icloud.com>
2025-06-11 16:27:39 +0300

    fix: middleware matcher now excludes logo.svg and small-logo.svg

--- src/middleware.ts
+++ src/middleware.ts
@@ -128,7 +128,8 @@ export const config = {
      * - _next/static (static files)
      * - _next/image (image optimization files)
      * - favicon.ico, sitemap.xml, robots.txt (metadata files)
+     * - logo.svg, small-logo.svg
      */
-    "/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)",
+    "/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|logo.svg|small-logo.svg).*)",
   ],
 };
