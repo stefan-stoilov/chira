7dc3d6b6904767d4a9b5c53a881c9c6532c65039
Stefan Stoilov <stefanstoilov11@icloud.com>
2025-06-11 17:27:26 +0300

    fix: fix for deployment api calls

--- src/lib/hc.ts
+++ src/lib/hc.ts
@@ -3,7 +3,11 @@ import type { OpenAPIHono } from "@hono/zod-openapi";
 import type { AppBindings } from "@/server/lib/types";
 
 export function hcInit<T extends OpenAPIHono<AppBindings, {}, "/">>() {
-  const rpc = hc<T>(process.env.NEXT_PUBLIC_APP_URL!);
+  const rpc = hc<T>(
+    process.env.NODE_ENV === "production"
+      ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+      : `${process.env.NEXT_PUBLIC_APP_URL}`,
+  );
 
   return { rpc };
 }
