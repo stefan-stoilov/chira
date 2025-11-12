1e0cd9642b4c0885b96ac13037d9c5831a73901e
Stefan Stoilov <stefanstoilov11@icloud.com>
2025-11-12 15:26:45 +0200

    chore: configuring e2e reporting for local and ci

--- playwright.config.ts
+++ playwright.config.ts
@@ -21,7 +21,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: "json",
+  reporter: CI ? [["json", { outputFile: "results.json" }]] : "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     baseURL,
